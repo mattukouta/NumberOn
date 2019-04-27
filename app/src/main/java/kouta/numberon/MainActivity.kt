0057721ce486b5fd6abb81393713adb314c0f1bd
Kouta Takamatsu <bb1016181@gmail.com>
2019-04-27 22:54:21 +0900

    SettingActivityの削除

--- app/src/main/java/kouta/numberon/MainActivity.kt
+++ app/src/main/java/kouta/numberon/MainActivity.kt
@@ -1,6 +1,5 @@
 package kouta.numberon
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -9,7 +8,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        startActivity(Intent(this, SettingActivity::class.java))
     }
 }
