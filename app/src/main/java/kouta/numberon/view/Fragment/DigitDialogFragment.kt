e43a5646e73db0e3633054588d6f7a4e171579b3
Kouta Takamatsu <bb1016181@gmail.com>
2019-04-29 10:22:42 +0900

    Dialogの表示場所を遷移後のActivityに変更、バックキーと背景タップ時を無効

--- app/src/main/java/kouta/numberon/view/Fragment/DigitDialogFragment.kt
+++ app/src/main/java/kouta/numberon/view/Fragment/DigitDialogFragment.kt
@@ -1,20 +1,18 @@
 package kouta.numberon.view.Fragment
 
 import android.app.Dialog
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.widget.Button
+import android.widget.RadioGroup
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.FragmentManager
 
 import kouta.numberon.R
 
 
 class DigitDialogFragment : DialogFragment() {
+
 
     override fun onCreateDialog(savedInstanceState : Bundle?) : Dialog {
         super.onCreateDialog(savedInstanceState)
@@ -24,6 +22,46 @@ class DigitDialogFragment : DialogFragment() {
 
         alert?.setView(view)
 
+
+        val cancel = view?.findViewById<Button>(R.id.cancel)
+        val ok = view?.findViewById<Button>(R.id.ok)
+        val radio = view?.findViewById<RadioGroup>(R.id.radioGroup)
+
+
+        cancel?.setOnClickListener { activity?.finish() }
+        ok?.setOnClickListener {
+            when (radio?.checkedRadioButtonId) {
+                R.id.two -> dismiss()
+                R.id.three -> dismiss()
+                R.id.four -> dismiss()
+                R.id.five -> dismiss()
+            }
+        }
+
         return alert?.create()!!
+    }
+
+    override fun show(manager : FragmentManager, tag : String?) {
+        showNow(manager, tag)
+    }
+
+    override fun showNow(manager : FragmentManager, tag : String?) {
+        if (isSameTagDialogShowing(manager, tag.toString())) return
+        super.showNow(manager, tag)
+    }
+
+    /**
+     * 同一TAGのダイアログが表示されているかどうかを判定する。
+     * @return 同一TAGのダイアログが表示されている場合は true を返す。
+     */
+    private fun isSameTagDialogShowing(manager : FragmentManager, tag : String) : Boolean {
+        val previousFragment = manager.findFragmentByTag(tag)
+        if (previousFragment is DialogFragment) {
+            val dialog = (previousFragment as DialogFragment).getDialog()
+            if (dialog != null && dialog.isShowing()) {
+                return true
+            }
+        }
+        return false
     }
 }
--- app/src/main/java/kouta/numberon/view/activity/SelectMode.kt
+++ app/src/main/java/kouta/numberon/view/activity/SelectMode.kt
@@ -1,10 +1,10 @@
 package kouta.numberon.view.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 import kouta.numberon.R
-import kouta.numberon.view.Fragment.DigitDialogFragment
 
 class SelectMode : AppCompatActivity() {
 
@@ -12,9 +12,16 @@ class SelectMode : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        local_btn.setOnClickListener {
-//            val dialogFragment = DigitDialogFragment()
-//            dialogFragment.show(supportFragmentManager, "alert")
-//        }
+        cpu_btn.setOnClickListener {
+//            dialogFragment.show(supportFragmentManager, "cpu")
+        }
+        local_btn.setOnClickListener {
+            val intent = Intent(this, OrderActivity::class.java)
+            intent.putExtra("Mode", "local")
+            startActivity(intent)
+        }
+        online_btn.setOnClickListener {
+//            dialogFragment.show(supportFragmentManager, "online")
+        }
     }
 }
