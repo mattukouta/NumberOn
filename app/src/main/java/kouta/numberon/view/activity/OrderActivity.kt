a5f50d50d5e3c7b46590129ffe3af1c7fa4e15a2
Kouta Takamatsu <bb1016181@gmail.com>
2019-04-30 03:02:26 +0900

    order_Card選択ロジック実装

--- app/src/main/java/kouta/numberon/view/activity/OrderActivity.kt
+++ app/src/main/java/kouta/numberon/view/activity/OrderActivity.kt
@@ -1,5 +1,6 @@
 package kouta.numberon.view.activity
 
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -7,11 +8,19 @@ import kotlinx.android.synthetic.main.activity_order.*
 import kouta.numberon.R
 import kouta.numberon.view.Fragment.DigitDialogFragment
 import android.view.View
+import android.widget.Toast
+import androidx.core.content.res.ResourcesCompat
 
 
 class OrderActivity : AppCompatActivity(), View.OnClickListener {
 
     val dialogFragment = DigitDialogFragment()
+    var view : View? = null
+    var player1 = 10
+    var player2 = 10
+    var select = 10
+    var card_base = R.drawable.trump_re
+    var select_card = R.drawable.trump_re_green
 
     override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,20 +46,43 @@ class OrderActivity : AppCompatActivity(), View.OnClickListener {
         seven.setOnClickListener(this)
         eight.setOnClickListener(this)
         nine.setOnClickListener(this)
+        select_btn.setOnClickListener(this)
     }
 
     override fun onClick(v : View?) {
         when (v) {
-            zero -> Log.d("check", "hoge")
-            one -> Log.d("check", "hoge")
-            two -> Log.d("check", "hoge")
-            three -> Log.d("check", "hoge")
-            four -> Log.d("check", "hoge")
-            five -> Log.d("check", "hoge")
-            six -> Log.d("check", "hoge")
-            seven -> Log.d("check", "hoge")
-            eight -> Log.d("check", "hoge")
-            nine -> Log.d("check", "hoge")
+            zero -> select = 0
+            one -> select = 1
+            two -> select = 2
+            three -> select = 3
+            four -> select = 4
+            five -> select = 5
+            six -> select = 6
+            seven -> select = 7
+            eight -> select = 8
+            nine -> select = 9
+            select_btn -> {
+                if (select != 10) {
+                    if (player1 == 10 && player2 == 10) {
+                        player1 = select
+                        view?.isEnabled = false
+                        view = null
+                        select_card = R.drawable.trump_re_red
+                    } else if (player1 != 10 && player2 == 10) {
+                        player2 = select
+                        Toast.makeText(this, "Player1:$player1, Player2:$player2", Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }
+
+        if (v != null && v != select_btn) {
+
+            if (view != null && v != view) {
+                view?.background = ResourcesCompat.getDrawable(resources, card_base, null)
+            }
+            v.background = ResourcesCompat.getDrawable(resources, select_card, null)
+            view = v
         }
     }
 
