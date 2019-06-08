2bcb418979be356b7e5d3eece49713a462a821f4
Kouta Takamatsu <bb1016181@gmail.com>
2019-06-08 18:40:20 +0900

    GameResultのContractの追加

---
+++ app/src/main/java/kouta/numberon/Presenter/Contract/GameResultContract.kt
@@ -0,0 +1,14 @@
+package kouta.numberon.Presenter.Contract
+
+import kouta.numberon.Presenter.BasePresenter
+import kouta.numberon.Presenter.BaseView
+
+interface GameResultContract {
+    interface View : BaseView<Presenter> {
+
+    }
+
+    interface Presenter : BasePresenter {
+
+    }
+}
--- app/src/main/java/kouta/numberon/Presenter/fragment/GameResultPresenter.kt
+++ app/src/main/java/kouta/numberon/Presenter/fragment/GameResultPresenter.kt
@@ -1,4 +1,9 @@
 package kouta.numberon.Presenter.fragment
 
-class GameResultPresenter {
+import kouta.numberon.Presenter.Contract.GameResultContract
+
+class GameResultPresenter : GameResultContract.Presenter {
+    override fun start() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
