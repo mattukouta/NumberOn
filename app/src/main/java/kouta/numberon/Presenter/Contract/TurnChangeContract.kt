9c7a9f2a8ead24632028e6887b5a5e21daf25a2b
Kouta Takamatsu <bb1016181@gmail.com>
2019-06-08 18:42:25 +0900

    TurnChangeのContractの追加

---
+++ app/src/main/java/kouta/numberon/Presenter/Contract/TurnChangeContract.kt
@@ -0,0 +1,14 @@
+package kouta.numberon.Presenter.Contract
+
+import kouta.numberon.Presenter.BasePresenter
+import kouta.numberon.Presenter.BaseView
+
+interface TurnChangeContract {
+    interface View : BaseView<Presenter> {
+
+    }
+
+    interface Presenter : BasePresenter {
+
+    }
+}
